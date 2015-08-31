39e1e1931e4bf2e5dd6ce80c96319114e93e2898
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-08-31 20:02:21 +0800

    Revert "hm2014811: Build camera HAL"
    This reverts commit 7f37b4695df0aa3a0e5552caf9c051497d5a5b4f.

--- device.mk
+++ device.mk
@@ -90,11 +90,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8916 \
-    libmm-qcamera
-
 # Charger
 PRODUCT_PACKAGES += \
     charger \
